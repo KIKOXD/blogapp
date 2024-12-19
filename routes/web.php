382458da42218ff72b1f71e6cf949d098d537d67
rnto4635 <rnto4635@gmail.com>
2024-12-19 18:52:35 +0700

    Update admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Log;
 
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
