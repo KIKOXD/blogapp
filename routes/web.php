ac604821384000b57c192e2bb4d05dc3d525a26d
rnto4635 <rnto4635@gmail.com>
2024-12-20 12:48:31 +0700

    Register error

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,14 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Admin\DashboardController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
+// Ini Untuk Admin Dashboard
+Route::prefix('admin')->middleware(['auth', 'is_admin'])->group(function () {
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
